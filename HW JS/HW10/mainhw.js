1ff9502a2d7cc3c4e2482ef5413b57db18bfa5e3
Ylubinets <lubinec2@gmail.com>
2020-12-09 15:31:41 +0200

    up global
    up global

--- HW JS/HW10/mainhw.js
+++ HW JS/HW10/mainhw.js
@@ -10,17 +10,17 @@
            if(target.tagName === 'I') {
                if(target.previousElementSibling.getAttribute('type') === 'password') {
                    target.previousElementSibling.setAttribute('type', 'text')
-                   target.classList.toggle('fa-eye', false);
-                   target.classList.toggle('fa-eye-slash', true);
+                   target.classList.toggle('fa-eye');
+                   target.classList.toggle('fa-eye-slash');
                } else if (target.previousElementSibling.getAttribute('type') === 'text') {
                    target.previousElementSibling.setAttribute('type', 'password')
-                   target.classList.toggle('fa-eye-slash', false);
-                   target.classList.toggle('fa-eye',true);
+                   target.classList.toggle('fa-eye-slash');
+                   target.classList.toggle('fa-eye');
                }
            }
        } )
 
-        btn.addEventListener('click', () => {
+        btn.addEventListener('click', function (event)  {
             event.preventDefault();
             const input = document.getElementById('input').value;
             const inputRepeat = document.getElementById('repeat_input').value;
